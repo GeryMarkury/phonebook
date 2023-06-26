0ff1602f23e30c144aa4138743c84667d5458abf
Heorhii <gerythepatient@gmail.com>
2023-06-26 20:50:29 +0200

    Install Chakra UI

--- src/index.js
+++ src/index.js
@@ -6,15 +6,18 @@ import { PersistGate } from 'redux-persist/integration/react';
 import './index.css';
 import { Provider } from 'react-redux';
 import { store, persistor } from "./redux/store";
+import { ChakraProvider } from '@chakra-ui/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <ChakraProvider>
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
     <BrowserRouter basename="/goit-react-hw-08-phonebook">
       <App />
     </BrowserRouter>
     </PersistGate>
-    </Provider>
+      </Provider>
+      </ChakraProvider>
     </React.StrictMode>
 );
