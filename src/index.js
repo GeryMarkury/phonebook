2c7bbaee801cde31e39c2ea8c5f0f9da62fed0d4
Heorhii Markov <115098288+GeryMarkury@users.noreply.github.com>
2023-10-13 17:06:19 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <ChakraProvider theme={theme}>
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-    <BrowserRouter basename="/goit-react-hw-08-phonebook">
+    <BrowserRouter basename="/phonebook">
       <App />
     </BrowserRouter>
     </PersistGate>
